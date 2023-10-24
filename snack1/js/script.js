dc601d9be40fbf3958042995fe4169fa45b326ff
valeriobartoletti97 <valerio.bartoletti97@gmail.com>
2023-10-24 16:13:05 +0200

    snack 1 resolved with map() method

--- snack1/js/script.js
+++ snack1/js/script.js
@@ -1,52 +1,22 @@
-const guests = [
-    {
-        tableName: 'Vip',
-        guestName: 'Brad Pitt',
-        place: 1
-    },
-    {
-        tableName: 'Vip',
-        guestName: 'Johnny Depp',
-        place: 2
-    },
-    {
-        tableName: 'Vip',
-        guestName: 'Lady Gaga',
-        place: 3
-    },
-    {
-        tableName: 'Vip',
-        guestName: 'Cristiano Ronaldo',
-        place: 4
-    },
-    {
-        tableName: 'Vip',
-        guestName: 'Georgina Rodriguez',
-        place: 5
-    },
-    {
-        tableName: 'Vip',
-        guestName: 'Chiara Ferragni',
-        place: 6
-    },
-    {
-        tableName: 'Vip',
-        guestName: 'Fedez',
-        place: 7
-    },
-    {
-        tableName: 'Vip',
-        guestName: 'George Clooney',
-        place: 8
-    },
-    {
-        tableName: 'Vip',
-        guestName: 'Amal Clooney',
-        place: 9
-    },
-    {
-        tableName: 'Vip',
-        guestName: 'Maneskin',
-        place: 10
-    }
+const nameGuests = [
+    'Brad Pitt',
+     'Johnny Depp',
+     'Lady Gaga', 
+     'Cristiano Ronaldo', 
+     'Georgina Rodriguez', 
+     'Chiara Ferragni', 
+     'Fedez',
+     'George Clooney',
+     'Amal Clooney', 
+     'Maneskin'
 ];
+console.log(nameGuests)
+
+const guestsList = nameGuests.map((name,index)=>{
+    let guest = {};
+    guest.tableName = 'Vip';
+    guest.guestName = name;
+    guest.place = index + 1;
+    return guest;
+})
+console.log(guestsList)
